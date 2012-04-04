98427c7c21831b2e098b8bde40152bd494813197
Cameron Pickett <pickett.cj@gmail.com>
2012-04-04 13:13:55 -0700

    Fix bugs with play_video.php; Audio/Video Fixes
    Audio/Video now correctly works as specified in play_video.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <body>
         <h1>UW UbiComp Lab TV Services</h1>
 
-        <form name="youtube" class="controlpanel" action="play_video.php" method="post">
+        <form name="youtube" class="controlpanel" method="post" action="play_video.php">
             Play YouTube Video: <input type="text" name="youtubeurl" /> <button action="submit" />
         </form>
     </body>
--- play_video.php
+++ play_video.php
@@ -1,12 +1,12 @@
-<!-- REQUIRES: youtube-dl, mplayer -->
+<!--
+    REQUIRES: youtube-dl, mplayer
+    In order to see/hear video, user logged in must be same as Apache server user as defined in /etc/apache2/envvars
+-->
 
 <?php
 
     $youtubeUrl = $_POST["youtubeurl"];
     $cookiesFile = "/var/tmp/youtube-dl-cookies.txt";
 
-    // FIXME: browser hangs at execution
-    $result = shell_exec('mplayer -cookies -cookies-file ' . $cookiesFile . ' $(youtube-dl -g --cookies ' . $cookiesFile . ' ' . $youtubeUrl . ')');
-
-    echo "<pre>$result</pre>";
+    exec('(mplayer -fs -cookies -cookies-file ' . $cookiesFile . ' $(youtube-dl -g --cookies ' . $cookiesFile . ' ' . $youtubeUrl . ')) >/dev/null </dev/null &');
 ?>
