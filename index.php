12b282638025c8cca8b4ba0d54e6b1ff72a28715
Cameron Pickett <pickett.cj@gmail.com>
2012-04-05 13:10:22 -0700

    Add changelog; Make index.php self-posting
    Add changelog for current version for milestones
    Change index.php to self-posting, so that user returns to form after
     action triggers
    play_video.php clean up
    Add setup instructions to readme

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <!DOCTYPE html>
+
+<!--
+    TV PHP Media Server
+    (c) Copyright 2012 Patrick Larson, Cameron Pickett
+    UW CSE, UbiComp Research Group
+-->
+
 <html>
 
     <head>
@@ -6,11 +13,25 @@
     </head>
 
     <body>
+
+<?php
+    if(isset($_POST['youtubeurl'])) {
+        include 'play_video.php';
+?>
+
+        <pre>Loading YouTube video...</pre>
+
+<?php
+    }
+?>
+
         <h1>UW UbiComp Lab TV Services</h1>
 
-        <form name="youtube" class="controlpanel" method="post" action="play_video.php">
-            Play YouTube Video: <input type="text" name="youtubeurl" /> <button action="submit" />
+        <form name="youtube" class="controlpanel" method="post">
+            Play YouTube Video: <input type="text" name="youtubeurl" title="Paste YouTube URL here..." size="40" />
+            <button type="submit">PLAY</button>
         </form>
+
     </body>
 
 </html>
--- play_video.php
+++ play_video.php
@@ -1,12 +1,18 @@
 <!--
+    TV PHP Media Server
+    (c) Copyright 2012 Patrick Larson, Cameron Pickett
+    UW CSE, UbiComp Research Group
+
     REQUIRES: youtube-dl, mplayer
     In order to see/hear video, user logged in must be same as Apache server user as defined in /etc/apache2/envvars
 -->
 
 <?php
 
+    /* FIXME: User could inject shell code into form */
     $youtubeUrl = $_POST["youtubeurl"];
     $cookiesFile = "/var/tmp/youtube-dl-cookies.txt";
 
-    exec('(mplayer -fs -cookies -cookies-file ' . $cookiesFile . ' $(youtube-dl -g --cookies ' . $cookiesFile . ' ' . $youtubeUrl . ')) >/dev/null </dev/null &');
+    exec('(mplayer -fs -cookies -cookies-file ' . $cookiesFile . ' $(youtube-dl -g --cookies '
+            . $cookiesFile . ' ' . $youtubeUrl . ')) >/dev/null </dev/null &');
 ?>
