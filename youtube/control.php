e4828da06160a267c6671795dba444963fe4570f
Cameron Pickett <pickett.cj@gmail.com>
2012-04-12 08:30:57 -0700

    Fully implement mplayer control interface
    MPlayer iface to control playback of YouTube videos
    Allows:
        pause/resume, mute/unmute, seek, faster/slower playback,
        regular playback, volume control

--- youtube/control.php
+++ youtube/control.php
@@ -10,9 +10,49 @@
 
     switch($_POST['action']) {
     case 'PAUSE':
-        // XXX: Requires further testing
         exec('echo "pause" > ' . FIFO . ' &');
         $statusMessage = "Pausing YouTube video...";
+        break;
+    case 'MUTE':
+        exec('echo "mute 1" > ' . FIFO . ' &');
+        $statusMessage = "Muting YouTube video...";
+        break;
+    case 'UNMUTE':
+        exec('echo "mute 0" > ' . FIFO . ' &');
+        $statusMessage = "Unmuting YouTube video...";
+        break;
+    case 'GOTO':
+        $seekPercentage = $_POST['seek'];
+        exec('echo "seek ' . $seekPercentage . ' 1" > ' . FIFO . ' &');
+        $statusMessage = "Going to $seekPercentage% of the video...";
+        break;
+    case 'FAST':
+        # TODO: Check if speed value is 1.0 or 100
+        exec('echo "speed_mult 1.25" > ' . FIFO . ' &');
+        $statusMessage = "Faster...";
+        break;
+    case 'FFAST':
+        exec('echo "speed_mult 2.0" > ' . FIFO . ' &');
+        $statusMessage = "Much faster...";
+    case 'SLOW':
+        exec('echo "speed_mult 0.8" > ' . FIFO . ' &');
+        $statusMessage = "Slower...";
+        break;
+    case 'SSLOW':
+        exec('echo "speed_mult 0.5" > ' . FIFO . ' &');
+        $statusMessage = "Much slower...";
+        break;
+    case 'REG':
+        exec('echo "speed_set 1.0" > ' . FIFO . ' &');
+        $statusMessage = "Normal speed...";
+        break;
+    case 'VOLUP':
+        exec('echo "volume 10 0" > ' . FIFO . ' &');
+        $statusMessage = "Increasing volume...";
+        break;
+    case 'VOLDOWN':
+        exec('echo "volume -10 0" > ' . FIFO . ' &');
+        $statusMessage = "Decreasing volume...";
         break;
     }
 
