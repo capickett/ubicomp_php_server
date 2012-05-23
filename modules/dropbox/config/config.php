08fc323e6b0549dff9681f521248b4cc454e5de8
Cameron Pickett <pickett.cj@gmail.com>
2012-05-22 17:30:23 -0700

    WIP: Adding Dropbox support

---
+++ modules/dropbox/config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+##
+# [DROPBOX_ROOT]
+#     Path to the dropbox root directory
+##
+
+$DROPBOX_ROOT = "/path/to/dropbox";
+
+##
+# [RECEIVE_FOLDER]
+#     Relative path of folder for receiving files
+##
+
+$RECEIVE_FOLDER = "dropittome";
---
+++ modules/dropbox/form.php
@@ -0,0 +1,5 @@
+<fieldset>
+    <legend>Dropbox</legend>
+
+    <h2>Recently Received <code>(Last updated <?= date("g:ia") ?>)</code>:</h2>
+</fieldset>
